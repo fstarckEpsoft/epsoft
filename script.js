e8ac80fd743e858c5828887378fad551c3b27e1c
fstarckEpsoft <felipe.starck@epsoft.com.br>
2024-04-16 19:41:29 -0300

    Adicionado responsividade na navbar e criado menu burguer

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 /* *** CRIA UM EFEITO DE ROLAGEM SUAVE AO CLICAR NOS LINKS DA PÁGINA *** */
 $(document).ready(function () {
   $('a[href^="#"]').on('click', function (event) {
-    var target = $(this.getAttribute('href'));
+    let target = $(this.getAttribute('href'));
     if (target.length) {
       event.preventDefault();
       $('html, body').stop().animate({
@@ -17,25 +17,25 @@ document.getElementById("ano-atual").textContent = new Date().getFullYear();
 
 
 function mostrarConteudo(conteudo) {
-  var divsImagens = document.querySelectorAll('.imagem-texto > div');
+  let divsImagens = document.querySelectorAll('.imagem-texto > div');
   divsImagens.forEach(function (div) {
     div.style.display = 'none';
   });
 
-  var divSelecionada = document.querySelector('.' + conteudo);
+  let divSelecionada = document.querySelector('.' + conteudo);
   divSelecionada.style.display = 'block';
 
-  var menuItems = document.querySelectorAll('.menu-item');
+  let menuItems = document.querySelectorAll('.menu-item');
   menuItems.forEach(function (item) {
     item.classList.remove('active');
   });
 
-  var itemClicado = document.querySelector('[onclick="mostrarConteudo(\'' + conteudo + '\')"]');
+  let itemClicado = document.querySelector('[onclick="mostrarConteudo(\'' + conteudo + '\')"]');
   itemClicado.classList.add('active');
 }
 
 function toggleAnswer(id) {
-  var answer = document.getElementById(id + '-answer');
+  let answer = document.getElementById(id + '-answer');
   if (answer.style.display === 'none') {
     answer.style.display = 'block';
   } else {
@@ -45,3 +45,11 @@ function toggleAnswer(id) {
 
 
 
+function toggleMenu() {
+  let linksHeader = document.getElementById("linksHeader");
+  if (linksHeader.style.display === "block") {
+    linksHeader.style.display = "none";
+  } else {
+    linksHeader.style.display = "block";
+  }
+}
